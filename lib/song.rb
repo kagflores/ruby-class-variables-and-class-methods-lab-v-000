cf5550c1663fab6be8efca6d63a1ba2c7de6e3e6
Karen Flores <karen.ag.flores@gmail.com>
2018-05-22 07:00:52 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,65 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    v = 1
+    @@genres.each do |genre|
+      #binding.pry
+      if genre_hash.has_key?(genre)
+        v = genre_hash[genre] +=1
+        genre_hash[genre] = v
+        #binding.pry
+      else
+        #genre_hash = {genre => 1}
+        genre_hash[genre] = 1
+      end
+      #binding.pry
+      genre_hash
+    end
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    v = 1
+    @@artists.each do |artist|
+      #binding.pry
+      if artist_hash.has_key?(artist)
+        v = artist_hash[artist] +=1
+        artist_hash[artist] = v
+        #binding.pry
+      else
+        #genre_hash = {genre => 1}
+        artist_hash[artist] = 1
+      end
+      #binding.pry
+      artist_hash
+    end
+  end
+
+end
