3ba2ff710fac97f5cb328ebb62a5d10fea614a87
Karen Flores <karen.ag.flores@gmail.com>
2018-05-22 07:43:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
         #genre_hash = {genre => 1}
         genre_hash[genre] = 1
       end
-      #binding.pry
+      binding.pry
       genre_hash
     end
   end
@@ -54,7 +54,7 @@ class Song
       else
         artistlist[artist] = 1
       end
-      binding.pry
+      #binding.pry
       artistlist.sort_by{|k,v| v}
     end
   end
